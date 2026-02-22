e1800231cbfac2b9985329ddb670007770eadb70
sanghyun6 <sanghyunleeee@gmail.com>
2026-02-21 18:46:03 -0800

    lab finished

--- Ackermann.cpp
+++ Ackermann.cpp
@@ -2,16 +2,30 @@
 using namespace std;
 
 // Ackermann function definition
-unsigned long long ackermann(unsigned int m, unsigned int n) {
- // TODO: Complete the code.
+unsigned long long ackermann(unsigned int m, unsigned int n)
+{
+    // TODO: Complete the code.
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return ackermann(m - 1, 1);
+    }
+    else
+    {
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
 }
 
-int main() {
+int main()
+{
     unsigned int m, n;
     cout << "Enter m and n: ";
     cin >> m >> n;
 
-    cout << "Ackermann(" << m << ", " << n << ") = " 
+    cout << "Ackermann(" << m << ", " << n << ") = "
          << ackermann(m, n) << endl;
 
     return 0;
